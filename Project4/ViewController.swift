6475eafeb67c86f1c7fd25189052c71dc6393509
Sf1770 <sf1770@nyu.edu>
2017-12-20 00:40:13 -0500

    Refactor, make code more efficient

--- Project4/ViewController.swift
+++ Project4/ViewController.swift
@@ -12,6 +12,9 @@ import WebKit
 class ViewController: UIViewController,WKNavigationDelegate {
     
     var webView: WKWebView!
+    var progressView: UIProgressView!
+    var websites = ["apple.com",
+                    "hackingwithswift.com"]
     
     override func loadView() {
         webView = WKWebView()
@@ -21,26 +24,55 @@ class ViewController: UIViewController,WKNavigationDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil) //#keyPath like #selector allows the compiler to check that your code is correct, that there actually exist an estimatedProgress property
+        
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
-        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action:nil)
-        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
-        toolbarItems = [spacer,refresh]
-        navigationController?.isToolbarHidden = false
         
-        let url = URL(string: "https://www.hackingwithswift.com")!
+        progressView = UIProgressView(progressViewStyle: .default) //creates UIProgressView instance
+        progressView.sizeToFit() //sets layout size to fit content
+        let progressButton = UIBarButtonItem(customView: progressView) //wrap our UIProgressView into a UIBarButtonItem in order to place it on the toolbar.
+        
+        
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action:nil) //we create a new bar button item using a special system type, flexible space, no need for a target or action cause it can't be tapped
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload)) //creates the refresh button to be displayed on the toolbar
+        toolbarItems = [progressButton,spacer,refresh] //array of the things on the toolbar
+        navigationController?.isToolbarHidden = false //makes the toolbar shown
+        
+        let url = URL(string: "https://" + websites[0])!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
         
     }
     
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            progressView.progress = Float(webView.estimatedProgress)
+        }
+    }
+    
     @objc func openTapped() {
         let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
-        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
-        ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage))
+        for website in websites{
+            ac.addAction(UIAlertAction(title: website, style: .default, handler: openPage))
+        }
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
         present(ac, animated: true)
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.url
+        
+        if let host = url!.host{
+            for website in websites{
+                if host.range(of: website) != nil{
+                    decisionHandler(.allow)
+                    return
+                }
+            }
+        }
+        decisionHandler(.cancel)
     }
     
     func openPage(action: UIAlertAction){
